ea48abd35c74647560c74511034cc35d5f284c7c
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-07-14 13:35:47 +0530

    fix: remove undefined authLimiter from authRoutes.js to fix Render build

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,9 +3,7 @@ import { registerUser, loginUser } from '../controllers/authController.js';
 import { registerRules, loginRules, validate } from '../middlewares/validator.js';
 
 const router = express.Router();
- 
-router.use(authLimiter);
- 
+
 router.post('/register', registerRules, validate, registerUser);
 router.post('/login', loginRules, validate, loginUser);
 
