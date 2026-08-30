05b292dc3b58cf2ffb4d19501904ecdf4414b773
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-08-31 01:12:49 +0530

    sec: limit express json payload size to 10kb

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', apiRoutes);
