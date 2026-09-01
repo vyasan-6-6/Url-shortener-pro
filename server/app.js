6bcd6a266e266711594f88f457f8a58f8556f99d
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-09-01 13:22:44 +0530

    sec: limit urlencoded payload size to 10kb

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use('/', apiRoutes);
 
